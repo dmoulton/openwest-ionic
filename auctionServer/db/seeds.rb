ff5d2cf5155ada7a0e729b9e000166be175ab720
David Moulton <dave@themoultons.net>
2014-05-09 14:58:54 -0600

    fixes before presentation

--- auctionServer/db/seeds.rb
+++ auctionServer/db/seeds.rb
@@ -1,29 +1,34 @@
-Auction.create ({title:'Sombrero',description:'Hey, its a sombrero, what more do you need to know',image_src:'/img/sombrero.jpg',increment_bid:5.00,current_bid:25.0})
+Auction.delete_all
+
+Auction.create ({title:'Sombrero',
+  description:'Hey, its a sombrero, what more do you need to know',
+  image_src:'http://localhost:3000/images/sombrero.jpg',
+  increment_bid:5.00,current_bid:25.0})
 
 Auction.create ({
 title:'Viking Warrior',
 description:'I have it on good authority that this is what Ragnar Lothbrok really looked like',
-image_src:'/img/viking_warrior.jpg',
+image_src:'http://localhost:3000/images/viking_warrior.jpg',
 increment_bid:5.00,
 current_bid:300.0})
 
 Auction.create ({
 title:'Large Dog',
 description:'This is Tipper. We\'ll entertain all offers on this one.',
-image_src:'/img/tipper.jpg',
+image_src:'http://localhost:3000/images/tipper.jpg',
 increment_bid:5.00,
 current_bid:25.0})
 
 Auction.create ({
 title:'Small Dog',
 description:'This is Rose. She is about to have puppies any day now. We think they will be cute.',
-image_src:'/img/rose.jpg',
+image_src:'http://localhost:3000/images/rose.jpg',
 increment_bid:10.00,
 current_bid:250.0})
 
 Auction.create ({
 title:'Even Smaller Dog',
 description:'This is Jack. He is about to become a dad for the first time. He is the nicest dog you will ever meet.',
-image_src:'/img/jack.jpg',
+image_src:'http://localhost:3000/images/jack.jpg',
 increment_bid:10.00,
 current_bid:250.0})
